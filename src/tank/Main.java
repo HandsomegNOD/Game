65c3a4514d17d1869540a4b46cfdb10474826e98
y <741005067@qq.com>
2023-05-11 20:06:20 +0800

    启动类Main

---
+++ src/tank/Main.java
@@ -0,0 +1,14 @@
+package tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TankFrame tf = new TankFrame();
+
+    }
+}
